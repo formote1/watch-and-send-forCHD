936e9f5b37939c6658700e8bef95bc220d2397a8
formote1 <ozodov.motion@gmail.com>
2026-09-21 23:03:41 +0500

    Update and rename config.example.py to config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Get your API_ID and API_HASH from https://my.telegram.org
+#   -> "API development tools" -> create an app.
+
+API_ID = 0            # your API id (an integer) from my.telegram.org
+API_HASH = ""         # your API hash (a string) from my.telegram.org
+SESSION_NAME = "session"  # file name (without extension) for your local login session
